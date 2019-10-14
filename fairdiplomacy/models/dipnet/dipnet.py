a429173b927c34794674631c4c162a52d8db545f
Jonathan Gray <jsgray@fb.com>
2019-10-14 12:58:14 -0400

    dipnet: simple decoder

--- fairdiplomacy/models/dipnet/dipnet.py
+++ fairdiplomacy/models/dipnet/dipnet.py
@@ -3,6 +3,46 @@ import torch.nn.functional as F
 from torch import nn
 
 
+class DipNet(nn.Module):
+    def __init__(
+        self,
+        board_state_size,  # 35
+        prev_orders_size,  # 40
+        inter_emb_size,  # 120
+        power_emb_size,  # 7 (one-hot)
+        season_emb_size,  # 3 (one-hot)
+        num_blocks,  # 16
+        A,  # 81x81
+        orders_vocab_size,  # 13k
+    ):
+        super().__init__()
+        self.encoder = DipNetEncoder(
+            board_state_size,
+            prev_orders_size,
+            inter_emb_size,
+            power_emb_size,
+            season_emb_size,
+            num_blocks,
+            A,
+        )
+        self.decoder = SimpleDipNetDecoder(81 * inter_emb_size * 2, orders_vocab_size)
+
+    def forward(self, x_bo, x_po, power_emb, season_emb):
+        enc = self.encoder(x_bo, x_po, power_emb, season_emb)
+        enc = enc.reshape(enc.shape[0], -1)
+        y = self.decoder(enc)
+        return y
+
+
+class SimpleDipNetDecoder(nn.Module):
+    def __init__(self, encoder_out_size, orders_vocab_size):
+        super().__init__()
+        self.linear = nn.Linear(encoder_out_size, orders_vocab_size)
+
+    def forward(self, x):
+        return self.linear(x)
+
+
 class DipNetEncoder(nn.Module):
     def __init__(
         self,
--- fairdiplomacy/models/dipnet/train_sl.py
+++ fairdiplomacy/models/dipnet/train_sl.py
@@ -1,20 +1,45 @@
 import argparse
+import logging
 import torch
 
 from fairdiplomacy.data.dataset import Dataset
 from fairdiplomacy.models.consts import ADJACENCY_MATRIX
-from dipnet import DipNetEncoder
+from dipnet import DipNet
+from order_vocabulary import get_order_vocabulary
+
+logging.basicConfig(format="%(asctime)s [%(levelname)s]: %(message)s", level=logging.DEBUG)
+
+BOARD_STATE_SIZE = 35
+PREV_ORDERS_SIZE = 40
+INTER_EMB_SIZE = 120
+POWER_EMB_SIZE = 7
+SEASON_EMB_SIZE = 3
+NUM_ENCODER_BLOCKS = 16
+ORDER_VOCABULARY = get_order_vocabulary()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--num-dataloader-workers", type=int, default=8)
-    parser.add_argument("--batch-size", type=int, default=8)
+    parser.add_argument("--num-dataloader-workers", type=int, default=8, help="# Dataloader procs")
+    parser.add_argument("--batch-size", type=int, default=8, help="How many GAMES (not moves)")
+    parser.add_argument("--lr", type=float, default=0.0001, help="Learning rate")
+    parser.add_argument("--model-out", help="Path to save the model")
     args = parser.parse_args()
 
     A = torch.from_numpy(ADJACENCY_MATRIX).float()
-    net = DipNetEncoder(35, 40, 120, 7, 3, 16, A)
 
-    print("Loading dataset...")
+    logging.info("Init model...")
+    net = DipNet(
+        BOARD_STATE_SIZE,
+        PREV_ORDERS_SIZE,
+        INTER_EMB_SIZE,
+        POWER_EMB_SIZE,
+        SEASON_EMB_SIZE,
+        NUM_ENCODER_BLOCKS,
+        A,
+        len(ORDER_VOCABULARY),
+    )
+
+    logging.info("Loading dataset...")
     dataset = Dataset(
         ["/Users/jsgray/code/fairdiplomacy/fairdiplomacy/data/out/game_3232.json"] * 16
     )
@@ -22,16 +47,21 @@ if __name__ == "__main__":
         dataset, shuffle=True, num_workers=args.num_dataloader_workers, batch_size=args.batch_size
     )
 
+    loss_fn = torch.nn.BCEWithLogitsLoss()
+    optim = torch.optim.Adam(net.parameters(), lr=args.lr)
+
     for batch_i, batch in enumerate(dataloader):
-        print("Starting batch {}".format(batch_i))
+        logging.info("Starting batch {}".format(batch_i))
 
         for i, tensor in enumerate(batch):
             batch[i] = tensor.reshape(-1, *tensor.shape[2:])  # flatten batch of batches
 
         x_state, x_orders, x_power, x_season, y_actions = batch
 
+        optim.zero_grad()
         y_guess = net(x_state, x_orders, x_power, x_season)
+        loss = loss_fn(y_guess, y_actions)
+        loss.backward()
+        optim.step()
 
-        print(y_guess.shape)
-
-        break
+        logging.info("batch {} loss={}".format(batch_i, loss))
