e314682d83ef74f87891602e9fddb04ff0a7a8a8
Jonathan Gray <jsgray@fb.com>
2019-10-15 13:56:57 -0700

    document BaseAgent interface

--- fairdiplomacy/agents/base_agent.py
+++ fairdiplomacy/agents/base_agent.py
@@ -1,3 +1,16 @@
+from typing import List
+
+
 class BaseAgent:
-    def get_orders(self, game, power):
+    def get_orders(self, game, power) -> List[str]:
+        """Return a list of orders that should be taken based on the game state
+
+        Arguments:
+        - game: a diplomacy.Game object
+        - power: str, one of {'AUSTRIA', 'ENGLAND', 'FRANCE', 'GERMANY',
+                              'ITALY', 'RUSSIA', 'TURKEY'}
+
+        Returns a list of order strings, e.g.
+            ["A TYR - TRI", "F ION - TUN", "A VEN S A TYR - TRI"]
+        """
         raise NotImplementedError("Subclasses must implement")
