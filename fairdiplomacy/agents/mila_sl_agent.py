110b2a3f5bbacaf9926e084e2ea97b03414a2192
Jonathan Gray <jsgray@fb.com>
2020-01-23 15:12:03 -0800

    mila_sl_agent: connect to jgray's devfair

--- fairdiplomacy/agents/mila_sl_agent.py
+++ fairdiplomacy/agents/mila_sl_agent.py
@@ -1,14 +1,13 @@
 import asyncio
-import os
-import random
-import sys
 import threading
 import queue
 
 import diplomacy
 from tornado import gen
 from tornado.platform.asyncio import AnyThreadEventLoopPolicy
-from diplomacy_research.players.benchmark_player import DipNetSLPlayer
+from diplomacy_research.models.datasets.grpc_dataset import GRPCDataset
+from diplomacy_research.players.benchmarks import sl_neurips2019
+from diplomacy_research.players.model_based_player import ModelBasedPlayer
 from diplomacy_research.utils.cluster import start_io_loop
 
 from fairdiplomacy.agents.base_agent import BaseAgent
@@ -32,7 +31,7 @@ class MilaSLAgent(BaseAgent):
 def thread_main(q_in, q_out, temperature):
     @gen.coroutine
     def coroutine():
-        player = DipNetSLPlayer()
+        player = Player()
         while True:
             game, power = q_in.get()
             orders = yield player.get_orders(game, power, temperature=temperature)
@@ -40,6 +39,21 @@ def thread_main(q_in, q_out, temperature):
 
     asyncio.set_event_loop_policy(AnyThreadEventLoopPolicy())
     start_io_loop(coroutine)
+
+
+class Player(ModelBasedPlayer):
+    def __init__(self, temperature=0.1, host="devfair0304", port=9501):
+        grpc_dataset = GRPCDataset(
+            hostname=host,
+            port=port,
+            model_name="player",
+            signature=sl_neurips2019.PolicyAdapter.get_signature(),
+            dataset_builder=sl_neurips2019.BaseDatasetBuilder(),
+        )
+        policy_adapter = sl_neurips2019.PolicyAdapter(grpc_dataset)
+
+        # Building benchmark model
+        super().__init__(policy_adapter=policy_adapter, temperature=temperature, use_beam=False)
 
 
 if __name__ == "__main__":
