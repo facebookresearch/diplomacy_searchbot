eaac172c728abe80ddb06ed5c8fe417944d46cb1
Anton Bakhtin <7470747+akhti@users.noreply.github.com>
2020-03-10 11:10:18 -0700

    Update fair_activate to build postman from the third_party

--- fair_activate.sh
+++ fair_activate.sh
@@ -19,10 +19,15 @@ if ! conda env list --json | jq ".envs | .[]" | grep -qE "/${xxx_env_name}\""; t
     conda install -y nodejs
     git submodule init
     git submodule update
+    # Install pytorch and patch cmake files so that things, e.g., postman, could be built against torch.
+    conda install --yes pytorch=1.4 torchvision cudatoolkit=9.2 -c pytorch
+    sed -i -e 's#/usr/local/cuda/lib64/libnvToolsExt.so#/public/apps/cuda/9.2/lib64/libnvToolsExt.so#g' $CONDA_PREFIX/lib/python3.7/site-packages/torch/share/cmake/Caffe2/Caffe2Targets.cmake
+    sed -i -e 's#/usr/local/cuda/lib64/libcudart.so#/public/apps/cuda/9.2/lib64/libcudart.so#g' $CONDA_PREFIX/lib/python3.7/site-packages/torch/share/cmake/Caffe2/Caffe2Targets.cmake
+    sed -i -e 's#/usr/local/cuda/lib64/libculibos.a#/public/apps/cuda/9.2/lib64/libculibos.a#g' $CONDA_PREFIX/lib/python3.7/site-packages/torch/share/cmake/Caffe2/Caffe2Targets.cmake
     pip install -e . -vv
     pip install -e ./thirdparty/github/fairinternal/postman/nest/
+    pip install -e ./thirdparty/github/fairinternal/postman/src/postman/
     pip install -e "git+ssh://git@github.com/fairinternal/submitit@master#egg=submitit"
-    pip install /checkpoint/hnr/wheels/postman-0.1.1-cp37-cp37m-linux_x86_64.whl
 fi
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
 export OMP_NUM_THREADS=1
