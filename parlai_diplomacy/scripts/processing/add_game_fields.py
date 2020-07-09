9617004a9257a93f4e7e1faf2670903ff41e85e1
apjacob <apjacob@fb.com>
2020-07-09 15:29:09 -0400

    Updates preprocessing script

--- parlai_diplomacy/scripts/processing/add_game_fields.py
+++ parlai_diplomacy/scripts/processing/add_game_fields.py
@@ -5,21 +5,18 @@
 # LICENSE file in the root directory of this source tree.
 
 """
-Simple script that preprocesses redacted_messages jsons and adds
+Last Run: July 7, 2020
+Simple script that preprocesses redacted_messages jsons and adds 
 fields from the the master sqlite table
 """
-
 import sqlite3
 import json
 import os
 import argparse
-
 from tqdm import tqdm
 from glob import glob
 
-
 DB2JSON_FIELD_TUPLE = ("hashed_id", "hashed_gameID")
-CHUNK_DATAPATH = '/checkpoint/fairdiplomacy/chat_messages_jsons/large_jsons_nostream/redacted_messages_*.json'
 
 
 def dict_factory(cursor, row):
@@ -29,19 +26,22 @@ def dict_factory(cursor, row):
     return d
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
     parser.add_argument("field", type=str)
     parser.add_argument("table", type=str)
     parser.add_argument(
-        "--save_dir",
+        "--datapath",
         type=str,
-        default="/checkpoint/fairdiplomacy/processed_chat_jsons/variantID",
+        default="/checkpoint/fairdiplomacy/chat_messages_json_runthree/"
+        "large_jsons_nostream/redacted_messages_*.json",
     )
     parser.add_argument(
-        "--db_path",
-        type=str,
-        default="/checkpoint/fairdiplomacy/facebook_notext.sqlite3",
+        "--save_dir", type=str, default="/checkpoint/fairdiplomacy/processed_chat_jsons/variantID"
+    )
+    parser.add_argument(
+        "--db_path", type=str, default="/checkpoint/fairdiplomacy/facebook_notext.sqlite3"
     )
     args = parser.parse_args()
 
@@ -51,26 +51,24 @@ if __name__ == '__main__':
     cur = conn.cursor()
     cur.execute(f"select {DB2JSON_FIELD_TUPLE[0]}, {args.field} from {args.table}")
     hashedID2field_list = cur.fetchall()
-    hashedID2field_dict = {
-        d[DB2JSON_FIELD_TUPLE[0]]: d[args.field] for d in hashedID2field_list
-    }
+    hashedID2field_dict = {d[DB2JSON_FIELD_TUPLE[0]]: d[args.field] for d in hashedID2field_list}
     print("Fields fetched...")
     conn.close()
 
-    tot = len(glob(CHUNK_DATAPATH))
-    for i, fle in enumerate(glob(CHUNK_DATAPATH)):
-        print(f'[ Processing data from path {i} / {tot}: {fle} ... ]')
-        with open(fle, 'r') as f:
+    tot = len(glob(args.datapath))
+    for i, fle in enumerate(glob(args.datapath)):
+        print(f"[ Processing data from path {i} / {tot}: {fle} ... ]")
+        with open(fle, "r") as f:
             json_data = json.load(f)
 
         print("Processing json...")
-        for json_entry in tqdm(json_data[2]['data']):
+        for json_entry in tqdm(json_data[2]["data"]):
             json_entry[f"{args.field}"] = str(
                 hashedID2field_dict[json_entry[DB2JSON_FIELD_TUPLE[1]]]
             )
 
         save_file = os.path.join(args.save_dir, os.path.basename(fle))
-        with open(save_file, 'w') as f:
+        with open(save_file, "w") as f:
             json.dump(json_data, f)
 
         print(f"Updated json saved to {save_file}")
