cfa2c6e3809c5bd0d3fc6344a57fdd7757c4b30a
Adam Lerer <alerer@fb.com>
2020-07-28 11:34:01 -0700

    updates to get_situation_check.py

--- bin/get_situation_results.py
+++ bin/get_situation_results.py
@@ -5,6 +5,7 @@ import json
 import re
 import glob
 import os
+import warnings
 from collections import Counter, defaultdict
 
 import tabulate
@@ -40,6 +41,7 @@ def print_situation_stats(paths):
                     "bp_prob": float(fields[5]),
                     "avg_u": float(fields[6]),
                     "regret": float(fields[6]) - avg_utility,
+                    "state_u": avg_utility,
                 }
             if "Result" in line:
                 result = fields[4]
@@ -47,7 +49,7 @@ def print_situation_stats(paths):
                 results[test][(game_idx, repeat)] = 1 if result == "PASSED" else 0
     # print(data)
     M = max(cfr_iters)
-    N = len(paths)
+    N = len(paths) // 2
     for power in POWERS:
         # print(f"{'='*20} {power:8} {'='*20}")
         # 1. calculate how different the plausible orders are
@@ -58,6 +60,7 @@ def print_situation_stats(paths):
                 data_j = data[(game_j, 0, power, M)]
                 common_orders = set(data_i.keys()) & set(data_j.keys())
                 # print(len(data_i), len(common_orders))
+                assert len(data_i) == len(data_j)
                 num_orders.append(len(data_i))
                 num_common_orders.append(len(common_orders))
                 common_prob.append(sum(data_i[o]["prob"] for o in common_orders))
@@ -69,35 +72,51 @@ def print_situation_stats(paths):
 
         # 2. calculate how consistent the values are between games, at different iters
         for cfr_iter in sorted(list(cfr_iters)):
-            prob_mse, u_mse, r_mse, avg_cr = [], [], [], []
-            for game_i in range(N):
-                for game_j in range(N):
-                    data_i = data[(game_i, 0, power, cfr_iter)]
-                    data_j = data[(game_j, 0, power, cfr_iter)]
-                    this_prob_mse, this_u_mse, this_r_mse, this_cr = 0, 0, 0, 0
-                    for action in data_i.keys():
-                        prob_i = data_i[action]["prob"]
-                        prob_j = data_j[action]["prob"]
-                        u_i = data_i[action]["avg_u"]
-                        u_j = data_j[action]["avg_u"]
-                        r_i = data_i[action]["regret"]
-                        r_j = data_j[action]["regret"]
-                        this_prob_mse += (prob_i - prob_j) ** 2
-                        this_u_mse += (prob_i + prob_j) / 2 * (u_i - u_j) ** 2
-                        this_r_mse += (prob_i + prob_j) / 2 * (r_i - r_j) ** 2
-                        this_cr += ((prob_j - prob_i) * u_j + (prob_i - prob_j) * u_i) / 2
+            prob_mse, u_mse, r_mse, avg_cr, order_prob_mse, state_u = [], [], [], [], [], []
+            for game in range(N):
+                data_i = data[(game, 0, power, cfr_iter)]
+                data_j = data[(game, 1, power, cfr_iter)]
+                this_prob_mse, this_u_mse, this_r_mse, this_cr, this_order_prob_mse = 0, 0, 0, 0, 0
+                order_probs_i, order_probs_j = defaultdict(float), defaultdict(float)
+                num_orders = 1
+                for action in data_i.keys():
+                    action_i = data_i[action]
+                    if action in data_j:
+                        action_j = data_j[action]
+                    else:
+                        warnings.warn(f"Did not find {action} in game {game}")
+                        action_j = action_i
+                    orders = eval(action)
+                    num_orders = len(orders)
+                    prob_i, u_i, r_i = action_i["prob"], action_i["avg_u"], action_i["regret"]
+                    prob_j, u_j, r_j = action_j["prob"], action_j["avg_u"], action_j["regret"]
+                    for order in orders:
+                        order_probs_i[order] += prob_i
+                        order_probs_j[order] += prob_j
 
-                    # print(
-                    #     f"{power:8s} {game} {cfr_iter:4d}: prob_rmse= {this_prob_mse**0.5:6.3f}  u_rmse= {this_u_mse**0.5:6.3f}  r_rmse=  {this_r_mse**0.5:6.3f}"
-                    # )
+                    this_prob_mse += (prob_i - prob_j) ** 2
+                    this_u_mse += (prob_i + prob_j) / 2 * (u_i - u_j) ** 2
+                    this_r_mse += (prob_i + prob_j) / 2 * (r_i - r_j) ** 2
+                    this_cr += ((prob_j - prob_i) * u_j + (prob_i - prob_j) * u_i) / 2
 
-                    prob_mse.append(this_prob_mse)
-                    u_mse.append(this_u_mse)
-                    r_mse.append(this_r_mse)
-                    avg_cr.append(this_cr)
+                for order in order_probs_i:
+                    this_order_prob_mse += (
+                        order_probs_i[order] - order_probs_j[order]
+                    ) ** 2 / num_orders
+
+                # print(
+                #     f"{power:8s} {game} {cfr_iter:4d}: prob_rmse= {this_prob_mse**0.5:6.3f}  u_rmse= {this_u_mse**0.5:6.3f}  r_rmse=  {this_r_mse**0.5:6.3f}"
+                # )
+
+                prob_mse.append(this_prob_mse)
+                u_mse.append(this_u_mse)
+                r_mse.append(this_r_mse)
+                avg_cr.append(this_cr)
+                order_prob_mse.append(this_order_prob_mse)
+                state_u.append(list(data_i.values())[0]["state_u"] if len(data_i) else 0)
 
             print(
-                f"{power:8s}  {cfr_iter:4d}: prob_rmse= {avg(prob_mse)**0.5:6.3f}  u_rmse= {avg(u_mse)**0.5:6.3f}  r_rmse=  {avg(r_mse)**0.5:7.4f}  avg_cr=  {avg(avg_cr):7.4f}"
+                f"{power:8s}  {cfr_iter:4d}: prob_rmse= {avg(prob_mse)**0.5:6.3f}  op_rmse= {avg(order_prob_mse)**0.5:6.3f}  u_rmse= {avg(u_mse)**0.5:6.3f}  r_rmse=  {avg(r_mse)**0.5:7.4f}  avg_cr=  {avg(avg_cr):7.4f}"
             )
 
     for test, r in results.items():
